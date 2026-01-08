569ec701067a6c4755b37c428c952e3f9021047c
Stephen Akande <tosisiyesteve@gmail.com>
2026-01-09 00:30:01 +0100

    task 2

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
+  def __str__(self):
+    return f"{self.title}"
+  
   title = models.CharField(max_length=200)
   author = models.CharField(max_length=100)
   publication_year = models.IntegerField()
