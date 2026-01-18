96a3f96473f60cb7d840ad3966490b17c1d7d884
Stephen Akande <tosisiyesteve@gmail.com>
2026-01-18 05:49:53 +0100

    task 0 second try

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,9 +4,9 @@ author = "a"
 author_obj = Author.objects.get(name = author)
 author_obj.books.objects.all()
 
-library = "library"
-library_obj = Library.objects.get(name = library)
-library_obj.books.objects.all()
+library_name = "library"
+library_obj = Library.objects.get(name = library_name)
+library_obj.books.all()
 
 librarian = "librarian"
 librarian_obj = Librarian.objects.get(name = librarian)
