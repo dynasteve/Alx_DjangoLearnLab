8529e405f3a6158fc8e9ab214ccdd5f0dc817bc9
Stephen Akande <tosisiyesteve@gmail.com>
2026-01-18 22:34:24 +0100

    task 2 first attempt

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import list_books, LibrayDetailView
+from .views import list_books, LibrayDetailView, UserLoginView, UserLogoutView, SignUpView
 #LibraryDetailView
 
-app_name='relationship_app'
+app_name='rel_app'
 urlpatterns = [
     path('fbv/', list_books, name='fbv_details'),
-    path('cbv/<int:pk>', LibrayDetailView.as_view(), name='cbv_details'),
+    path('cbv/<int:pk>/', LibrayDetailView.as_view(), name='cbv_details'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', SignUpView.as_view(), name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 from .models import Library, Book
 
 # Create your views here.
@@ -11,3 +15,15 @@ def list_books(request):
 class LibrayDetailView(DetailView):
   model = Library
   template_name = 'relationship_app/library_detail.html'
+  
+class UserLoginView(LoginView):
+  template_name = 'relationship_app/login.html'
+
+class UserLogoutView(LogoutView):
+  template_name = 'relationship_app/logout.html'
+  
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
