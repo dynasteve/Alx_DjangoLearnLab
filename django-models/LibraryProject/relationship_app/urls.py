855b4e59e46b48da354f5e11aaa044ecfbe5e8c9
Stephen Akande <tosisiyesteve@gmail.com>
2026-01-18 22:38:54 +0100

    task 2 second attempt

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ app_name='rel_app'
 urlpatterns = [
     path('fbv/', list_books, name='fbv_details'),
     path('cbv/<int:pk>/', LibrayDetailView.as_view(), name='cbv_details'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('login/', UserLoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
+    path('logout/', UserLogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout'),
     path('register/', SignUpView.as_view(), name='register'),
 ]
