0251a475fbc5bae04f38008d620d27127f9cddff
Stephen Akande <tosisiyesteve@gmail.com>
2026-01-18 22:37:34 +0100

    task 2 first attempt

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
