7a59d6c79e9cc00a6dafa8a33d2eb8cd4f315af7
Stephen Akande <tosisiyesteve@gmail.com>
2026-01-18 15:33:07 +0100

    task 1 fourth attempt

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibrayDetailView
 
 app_name='relationship_app'
 urlpatterns = [
-    path('fbv/', views.fbv_book_details, name='fbv_details'),
-    path('cbv/<int:pk>', views.CBV_library_details.as_view(), name='cbv_details'),
+    path('fbv/', list_books, name='fbv_details'),
+    path('cbv/<int:pk>', LibrayDetailView.as_view(), name='cbv_details'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,11 +3,11 @@ from django.views.generic.detail import DetailView
 from .models import Library, Book
 
 # Create your views here.
-def fbv_book_details(request):
+def list_books(request):
   books = Book.objects.all()
   template = 'relationship_app/list_books.html'
   return render(request, template, {'books':books})
 
-class CBV_library_details(DetailView):
+class LibrayDetailView(DetailView):
   model = Library
   template_name = 'relationship_app/library_detail.html'
