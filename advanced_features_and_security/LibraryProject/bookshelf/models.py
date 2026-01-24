1a6f65607f9f3cb8355ff122634614c6060a4a76
Stephen Akande <tosisiyesteve@gmail.com>
2026-01-24 05:32:10 +0100

    Task 0 second attempt

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager
 from .managers import CustomUserManager
 
 # Create your models here.
@@ -19,3 +20,41 @@ class CustomUser(AbstractUser):
   
   def __str__(self):
      return self.username
+   
+class CustomUserManager(UserManager):
+  """
+  Custom manager for CustomUser model
+  """
+  
+  def create_user(self, username, password=None, **kwargs):
+    if not username:
+        raise ValueError("Username must be set")
+
+    username = self.model.normalize_username(username)
+    
+    user = self.model(username=username, **kwargs)
+    
+    if password:
+        user.set_password(password)
+    
+    # Only default to False if not explicitly passed
+    if 'is_staff' not in kwargs:
+        user.is_staff = False
+    if 'is_superuser' not in kwargs:
+        user.is_superuser = False
+
+    user.save(using=self._db)
+    return user
+  
+  def create_superuser(self, username, password=None, **kwargs):
+    kwargs.setdefault("is_staff", True)
+    kwargs.setdefault("is_superuser", True)
+    kwargs.setdefault("is_active", True)
+
+    if kwargs.get("is_staff") is not True:
+        raise ValueError("Superuser must have is_staff=True")
+
+    if kwargs.get("is_superuser") is not True:
+        raise ValueError("Superuser must have is_superuser=True")
+
+    return self.create_user(username=username, password=password, **kwargs)
