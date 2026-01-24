687b8be4db41f951b0a5c462a04d091fa04c4e97
Stephen Akande <tosisiyesteve@gmail.com>
2026-01-24 05:33:04 +0100

    Task 0 third attempt

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import BaseUserManager
 from .managers import CustomUserManager
 
 # Create your models here.
@@ -21,7 +21,7 @@ class CustomUser(AbstractUser):
   def __str__(self):
      return self.username
    
-class CustomUserManager(UserManager):
+class CustomUserManager(BaseUserManager):
   """
   Custom manager for CustomUser model
   """
