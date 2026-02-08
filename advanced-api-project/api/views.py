156436515c1cd19399a43e9eedfcaa8d49d2bb4f
Stephen Akande <tosisiyesteve@gmail.com>
2026-02-08 18:44:20 +0100

    week 13 task 1, third attempt

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .serializers import *
 from .models import *
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 class BookListView(generics.ListAPIView):
