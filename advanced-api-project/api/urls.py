1100cc1337473c08e807938f03dd1fdfea6f04cb
Stephen Akande <tosisiyesteve@gmail.com>
2026-02-08 18:42:12 +0100

    week 13 task 1, first attempt

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+import views
+
+urlpatterns = [
+    path('books/', views.BookListView.as_view(), name='list_books'),
+    path('books/add_book', views.BookCreateView.as_view(), name='create_books'),
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book_detail'),
+    path('books/update/<int:pk>', views.BookUpdateView.as_view(), name='book_update'),
+    path('books/delete/<int:pk>', views.BookDeleteView.as_view(), name='book_delete'),
+]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import *
+from .models import *
 
 # Create your views here.
+class BookListView(generics.ListAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+  
+class BookDetailView(generics.RetrieveAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+  
+class BookCreateView(generics.CreateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+  
+  def perform_create(self, serializer):
+    serializer.save()
+
+class BookUpdateView(generics.UpdateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+  
+  def perform_update(self, serializer):
+    serializer.save()
+
+class BookDeleteView(generics.DestroyAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
