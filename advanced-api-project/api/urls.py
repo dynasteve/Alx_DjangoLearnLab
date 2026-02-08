b2394d85459c3953a40dbe84bacb7d5ffcac0220
Stephen Akande <tosisiyesteve@gmail.com>
2026-02-08 18:48:03 +0100

    week 13 task 1, fourth attempt

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-import views
+from .views import *
 
 urlpatterns = [
-    path('books/', views.BookListView.as_view(), name='list_books'),
-    path('books/create', views.BookCreateView.as_view(), name='create_books'),
-    path('books/<int:pk>', views.BookDetailView.as_view(), name='book_detail'),
-    path('books/update/<int:pk>', views.BookUpdateView.as_view(), name='book_update'),
-    path('books/delete/<int:pk>', views.BookDeleteView.as_view(), name='book_delete'),
+    path('books/', BookListView.as_view(), name='list_books'),
+    path('books/create', BookCreateView.as_view(), name='create_books'),
+    path('books/<int:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('books/update/<int:pk>', BookUpdateView.as_view(), name='book_update'),
+    path('books/delete/<int:pk>', BookDeleteView.as_view(), name='book_delete'),
 ]
