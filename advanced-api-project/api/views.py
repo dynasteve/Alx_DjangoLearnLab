96ae4c36988a0c0ee0624f7a037478e1505df02f
Stephen Akande <tosisiyesteve@gmail.com>
2026-02-08 23:47:24 +0100

    week 13 task 2, first attempt

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,13 +2,36 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .serializers import *
 from .models import *
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 # from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 class BookListView(generics.ListAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+  
+  filterset_fields = ['title', 'publication_year', 'author']
+  search_fields = ['title', 'author__name']
+  ordering_fields = ['title', 'publication_year']
+  ordering = ['title'] 
+  
+  def get_queryset(self):
+    queryset = Book.objects.all()
+    year = self.request.query_params.get('year')
+    author = self.request.query_params.get('author')
+    title = self.request.query_params.get('title')
+
+    if year:
+        queryset = queryset.filter(publication_year=year)
+    if author:
+        queryset = queryset.filter(author=author)
+    if title:
+        queryset = queryset.filter(title=title)
+
+    return queryset
   
 class BookDetailView(generics.RetrieveAPIView):
   queryset = Book.objects.all()
