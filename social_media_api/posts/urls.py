4d8f559796b9b676898680b52739dd063b8ce29e
Stephen Akande <tosisiyesteve@gmail.com>
2026-02-24 22:07:55 +0100

    Week 15 task 2 second

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,8 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, feed_view
 from django.urls import path
 
+# permissions.IsAuthenticated
+
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
